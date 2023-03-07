02f82c5b5bcadbe157283a5ce580814bebe053b1
Awcator <54628909+awcator@users.noreply.github.com>
2023-03-07 19:23:08 +0530

    Create quick_tcp_proxy.sh

---
+++ networking/quick_tcp_proxy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+
+if [ $# != 3 ]
+then
+    echo "usage: $0 <src-port> <dst-host> <dst-port>"
+    exit 0
+fi
+
+TMP=`mktemp -d`
+PIPE=$TMP/pipe
+trap 'rm -rf "$TMP"' EXIT
+mkfifo -m 0600 "$PIPE"
+
+nc -k -l -p "$1" <"$PIPE" | nc "$2" "$3" > "$PIPE"
+
+#usage ./quick_tcp_proxy.sh 5432 google.com 80
