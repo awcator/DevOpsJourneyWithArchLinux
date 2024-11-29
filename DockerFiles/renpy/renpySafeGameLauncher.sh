39f381b16db8c6c17b591021bc2a316b403b12d1
Awcator <devayya.sm@philips.com>
2024-11-29 15:31:54 +0530

    .

--- DockerFiles/renpy/renpySafeGameLauncher.sh
+++ DockerFiles/renpy/renpySafeGameLauncher.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 xhost +local:*
-docker run --device /dev/dri/ -it -v /run/user/1000/pulse:/tmp/run/user/973/pulse  -e SDL_AUDIODRIVER=pulseaudio -e  DISPLAY=$DISPLAY --net=none -v `pwd`:"/game/" -v /tmp/:/tmp/ -w /game awcator/renpygamelauncher bash -c "pulseaudio -D;$1"
+docker run --device /dev/dri/ -it   \
+	-v /run/user/$(id -u)/pulse:/run/user/1000/pulse:ro \
+    -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
+    --device /dev/snd \
+    -v ~/.renpy:/home/renpy/.renpy:rw \
+    -e  DISPLAY=$DISPLAY  -v `pwd`:"/game/":rw -v /tmp/:/tmp/ -w /game awcator/renpygamelauncher bash -c "pulseaudio -D;$1"
 #SDL_AUDIODRIVER
 #PULSE_SERVER=unix:/run/user/0/pulse/native
